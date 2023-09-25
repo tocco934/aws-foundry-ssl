3bd596cb6bb6de5f0651b426654373eee91ec1a5
Mike Hopkins <mike@dthought.net>
2023-09-25 10:23:44 +1000

    Actually don't need this

--- setup/certbot/certbot.sh
+++ setup/certbot/certbot.sh
@@ -27,15 +27,15 @@ if [ -d "/etc/letsencrypt/live/${subdomain}.${fqdn}" ]; then
     echo "Checking SSL certificate for renewal..."
 
     # Certificate exists, we can check if it needs renewal
-    certbot renew --nginx --no-random-sleep-on-renew --post-hook "systemctl restart nginx" > /var/log/foundrycron/certbot_renew.log 2>&1
+    certbot renew --nginx --no-random-sleep-on-renew --post-hook "systemctl restart nginx"
 else
     echo "SSL certificate not found, attempting to set it up..."
 
     # Try to fetch the certificates
-    certbot --agree-tos -n --nginx -d ${subdomain}.${fqdn} -m ${email} --no-eff-email > /var/log/foundrycron/certbot_renew.log 2>&1
+    certbot --agree-tos -n --nginx -d ${subdomain}.${fqdn} -m ${email} --no-eff-email
 
     # Install certificates for optional webserver
     if [[ ${webserver_bool} == 'True' ]]; then
-        certbot --agree-tos -n --nginx -d ${fqdn},www.${fqdn} -m ${email} --no-eff-email >> /var/log/foundrycron/certbot_renew.log 2>&1
+        certbot --agree-tos -n --nginx -d ${fqdn},www.${fqdn} -m ${email} --no-eff-email
     fi
 fi
